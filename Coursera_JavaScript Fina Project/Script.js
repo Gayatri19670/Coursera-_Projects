ee8b27f9f12e27d7e7f0d4c230884ad3d4a1081c
Samir Gayatri <you@example.com>
2021-04-13 21:58:01 +0530

    first commit

--- Coursera_JavaScript Fina Project/Script.js
+++ Coursera_JavaScript Fina Project/Script.js
@@ -1,32 +1,17 @@
-/*Add the JavaScript here for the function billingFunction(). It is responsible for setting and clearing
-the fields in Billing Information */
-function billingCopy(sameAsShipping) {
-    if (sameAsShipping.checked) {
-    var name = document.getElementById('shippingName').value;
-    var zip = document.getElementById('shippingZip').value;
-    } else {
-    name = '';
-    zip = '';
-    }
-    document.getElementById('billingName').value = name;
-    document.getElementById('billingZip').value = zip;
-    }
-    function billingCheck() {
-    // Checking the Name fields
-    var shipName = document.getElementById('shippingName');
-    var billName = document.getElementById('billingName');
-    if (shipName.value != billName.value) {
-    document.getElementById('name-alert').innerHTML = "Does the billing info not match?";
-    } else {
-    document.getElementById('name-alert').innerHTML = "The names match.";
-    }
-    // Checking the Zip Code fields
-    var shipZip = document.getElementById('shippingZip');
-    var billZip = document.getElementById('billingZip');if (shipZip.value != billZip.value) {
-    document.getElementById('zip-alert').innerHTML = "";
-    return false;
-    } else {
-    document.getElementById('zip-alert').innerHTML = "The zip codes match.";
-    return true;
-    }
-    }
+function billingFunction(){
+
+	if(document.querySelector("#same").checked)
+	{
+		var name=document.querySelector("#shippingName").value;   //this will copy the value entered by the user in shipping name block
+		var zip=document.querySelector("#shippingZip").value;     //this will copy the value entered by the user in shipping zip block
+
+		document.querySelector("#billingName").value=name;   //finally these lines will copy the above initialised variables to the billing address and billing zip
+		document.querySelector("#billingZip").value=zip;
+	}
+
+	else
+	{
+		document.querySelector("#billingName").value="";
+		document.querySelector("#billingZip").value="";
+	}
+}
